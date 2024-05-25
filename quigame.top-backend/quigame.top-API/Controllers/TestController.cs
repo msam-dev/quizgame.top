66457ff86e2451f6161adac8fadf6c14f0fcb551
Sam-Dev <sam.engineer0@gmail.com>
2024-05-26 09:59:13 +1000

    setting up asp.net project for creating api's

---
+++ quigame.top-backend/quigame.top-API/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace quizgame.top_API.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> Logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            Logger = logger;
+        }
+
+        [HttpGet("test-endpoint")]
+        public string Get()
+        {
+            Logger.Log(LogLevel.Information, "test-enpoint was called"); 
+            return "The time is: " + DateTime.UtcNow.ToString();
+        }
+    }
+}
