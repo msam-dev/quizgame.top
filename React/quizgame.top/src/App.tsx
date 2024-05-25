2166fd536f8880e9423a428951ee5c9e6f864e9b
Sam-Dev <sam.engineer0@gmail.com>
2024-05-25 12:17:09 +1000

    small change

--- React/quizgame.top/src/App.tsx
+++ React/quizgame.top/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-         count is {count}
+         count {count}
         </button>
       </div>
     </>
