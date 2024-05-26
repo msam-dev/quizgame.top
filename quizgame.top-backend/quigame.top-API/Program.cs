9254cd6fba0eb5878c4030a11d8c887138f20068
Sam-Dev <sam.engineer0@gmail.com>
2024-05-26 10:24:51 +1000

    changing file strcuture

---
+++ quizgame.top-backend/quigame.top-API/Program.cs
@@ -0,0 +1,23 @@
+
+namespace quizgame.top_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
