1842d6886f901b72fed903c9bbe46c54889fe7b1
msam-dev <sam.engineer0@gmail.com>
2024-06-02 09:10:54 +1000

    adding react experimentation

--- quizgame.top-frontend/quizgame.top/src/App.tsx
+++ quizgame.top-frontend/quizgame.top/src/App.tsx
@@ -1,18 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function increment(count: number): number{
+  //setCount(count+1);
+  return count+1;
+}
+
+
+function App() {
+const [count, setCount] = useState(0);
+const [apiCall, setApi] = useState("default");
+
+
+function getApiCall(): string {
+  let data:string = "";
+  //data = fetch('https://api.quizgame.top/api/test/test-endpoint', { method: 'GET' })
+  return data;
+}
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(increment(count))}>
          count {count}
         </button>
+      </div>
+
+      <div className="card">
+        <button onClick={getApiCall}>
+          {/* Click to Test Api: {apiCall} */}
+        </button> 
       </div>
     </>
   )
 }
 
-export default App
+export default App;
