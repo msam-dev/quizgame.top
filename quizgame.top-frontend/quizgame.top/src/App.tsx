eea141aa7abd4315def7a6c9f19193e6cfe669ea
mSam-Dev <sam.engineer0@gmail.com>
2024-06-02 11:12:38 +1000

    cleaning up landing page

--- quizgame.top-frontend/quizgame.top/src/App.tsx
+++ quizgame.top-frontend/quizgame.top/src/App.tsx
@@ -1,36 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
-
-function increment(count: number): number{
-  //setCount(count+1);
-  return count+1;
-}
-
-
 function App() {
-const [count, setCount] = useState(0);
-const [apiCall, setApi] = useState("default");
-
-
-function getApiCall(): string {
-  let data:string = "";
-  //data = fetch('https://api.quizgame.top/api/test/test-endpoint', { method: 'GET' })
-  return data;
-}
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount(increment(count))}>
-         count {count}
-        </button>
-      </div>
-
-      <div className="card">
-        <button onClick={getApiCall}>
-          {/* Click to Test Api: {apiCall} */}
-        </button> 
-      </div>
+      <div className="welcome-card">
+        Hello : )
+      </div>      
     </>
   )
 }
