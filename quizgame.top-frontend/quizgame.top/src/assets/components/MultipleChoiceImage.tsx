cafefa150ba216065b0df8ab908d6747fbaa7440
mSam-Dev <sam.engineer0@gmail.com>
2024-12-15 20:21:34 +1100

    answer checking

--- quizgame.top-frontend/quizgame.top/src/assets/components/MultipleChoiceImage.tsx
+++ quizgame.top-frontend/quizgame.top/src/assets/components/MultipleChoiceImage.tsx
@@ -1,15 +1,17 @@
 import '../css/MultipleChoiceImage.scss';
 
 interface MultipleChoiceImageProps {
+  answer:  string;
   answer1: string;
   answer2: string;
   answer3: string;
   answer4: string;
   imageUrl: string;
   next: () => void; 
+  submit: (index: string, answer: string) => void;
 }
 
-const MultipleChoiceImage = ({ answer1, answer2, answer3, answer4, imageUrl, next }: MultipleChoiceImageProps) => {
+const MultipleChoiceImage = ({ answer, answer1, answer2, answer3, answer4, imageUrl, next, submit }: MultipleChoiceImageProps) => {
   return (
     <div className='multiple-choice-outer-container'>
       <div className='multiple-choice-inner-container'>  
@@ -19,20 +21,19 @@ const MultipleChoiceImage = ({ answer1, answer2, answer3, answer4, imageUrl, nex
           </div>
         </div>
         <div className='multiple-choice-answer-container'>
-          <div className='multiple-choice-button'>
+          <div className='multiple-choice-button' onClick={() => submit(answer1, answer)}>
             {answer1}
           </div> 
-          <div className='multiple-choice-button'>
+          <div className='multiple-choice-button' onClick={() => submit(answer2, answer)}>
             {answer2}
           </div> 
-          <div className='multiple-choice-button'>
+          <div className='multiple-choice-button' onClick={() => submit(answer3, answer)}>
             {answer3}
           </div> 
-          <div className='multiple-choice-button'>
+          <div className='multiple-choice-button' onClick={() => submit(answer4, answer)}>
             {answer4}
           </div> 
         </div>
-
         <div className='multiple-choice-next-button' onClick={next}>
           Next Question
         </div>
--- quizgame.top-frontend/quizgame.top/src/pages/FlagQuiz.tsx
+++ quizgame.top-frontend/quizgame.top/src/pages/FlagQuiz.tsx
@@ -1,54 +1,64 @@
 import '../assets/css/FlagQuiz.scss';
+import countries from '../assets/data/Countries.json';
 import MultipleChoiceImage from '../assets/components/MultipleChoiceImage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const MultipleChoiceQuiz = () => {
 
-  const [country1, setCountry1] = useState(String);
-  const [country2, setCountry2] = useState(String);
-  const [country3, setCountry3] = useState(String);
-  const [country4, setCountry4] = useState(String);
-  const [flagImg,  setFlagImg]  = useState(String);
-  
-  const newQuestion = () => {
-    
-    const uniqueNumbers: Set<number> = new Set<number>;
+  useEffect(() => { newQuestion() }, []);
 
+  const [country1, setCountry1] = useState<string>("");
+  const [country2, setCountry2] = useState<string>("");
+  const [country3, setCountry3] = useState<string>("");
+  const [country4, setCountry4] = useState<string>("");
+  const [flagImg,  setFlagImg ] = useState<string>("");
+  const [answer,   setAnswer  ] = useState<string>("");
+
+  // 
+  const newQuestion = () => {
+
+    const uniqueNumbers: Set<number> = new Set<number>;
     while (uniqueNumbers.size < 4) {
-      const randomNumber: number = Math.floor(Math.random() * 207) + 1;
+      const randomNumber: number = Math.floor(Math.random() * countries.length);
       uniqueNumbers.add(randomNumber);
     }
 
     const arr: Array<number> = Array.from(uniqueNumbers);
-    setCountry1(arr[0].toString());
-    setCountry2(arr[1].toString());
-    setCountry3(arr[2].toString());
-    setCountry4(arr[3].toString());
+    setCountry1(countries[arr[0]].name);
+    setCountry2(countries[arr[1]].name);
+    setCountry3(countries[arr[2]].name);
+    setCountry4(countries[arr[3]].name);
 
-    const answer: number = Math.floor(Math.random() * 4) + 1;
-    
-
+    const index: number = Math.floor(Math.random() * 4);
+    setAnswer(countries[arr[index]].name);
+    setFlagImg(countries[arr[index]].image_url);
   };
+
+  const submit = (guess: string, correct: string, id: string) =>{
+    
+    console.log("guess:"+guess+" answer:"+correct+" correct?"+(guess == correct));
+  }
 
   return (
     <>
       <MultipleChoiceImage 
+        answer  ={answer}
         answer1 ={country1} 
         answer2 ={country2}
         answer3 ={country3}
         answer4 ={country4}
         imageUrl={flagImg}
         next    ={newQuestion}
+        submit  ={submit}
       />
     </>
   );
 }
-
 
 // answer1="Germany" 
 // answer2="England" 
 // answer3="Democratic Republic of Congo" 
 // answer4="China" 
 // imageUrl="https://upload.wikimedia.org/wikipedia/commons/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg"
-export default MultipleChoiceQuiz;
 
+export default MultipleChoiceQuiz;
