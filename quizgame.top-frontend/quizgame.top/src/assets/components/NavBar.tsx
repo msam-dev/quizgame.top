d0298f81f6fc08cdeaec59976f7ab1efa5c43a7c
mSam-Dev <sam.engineer0@gmail.com>
2024-12-31 11:28:49 +1100

    Adding functioning settings menu

--- quizgame.top-frontend/quizgame.top/src/assets/components/NavBar.tsx
+++ quizgame.top-frontend/quizgame.top/src/assets/components/NavBar.tsx
@@ -1,19 +1,39 @@
 import '../css/Navbar.scss';
+
 import title from '../images/quizgame-title-LD.png';
+import { useState } from 'react';
+import { AiTwotoneSetting } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const openSettings = () => {
+    setOpen(true);
+  };
+
+  const closeSettings = () => {
+    setOpen(false);
+  };
+
   return (
     <div className='navbar-container'>
       <div className="navbar-content">
         <div className='navbar-logo-container'>
           <a href='/'><img className='navbar-logo' src={title}/></a>
         </div>
-        <div className='navbar-settings-container'>
-          <a className='navbar-settings' href='/world-flag-quiz-mc'>Quiz</a>
-          <a className='navbar-settings' href='/test'>API</a>
-          <a className='navbar-settings' href='/settings'>Settings</a>
-        </div>
+        <div className='navbar-settings-icon-container'>
+          <AiTwotoneSetting className='navbar-settings-icon' onClick={openSettings}/>
+        </div> 
       </div>
+      <div className={`navbar-settings-drawer-container ${open}`}>
+        <div className={`navbar-settings-drawer-overlay ${open}`}></div>
+        <div className={`navbar-settings-drawer ${open}`}>
+          <AiOutlineClose className='navbar-settings-close-button' onClick={closeSettings}/>
+        </div>
+      </div> 
     </div> 
   );
 }
