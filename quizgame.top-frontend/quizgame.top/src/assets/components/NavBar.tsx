cca51124a87738591319393cba4ff533db543a28
mSam-Dev <sam.engineer0@gmail.com>
2024-12-31 15:32:07 +1100

    improvements to navbar settings popout

--- quizgame.top-frontend/quizgame.top/src/assets/components/NavBar.tsx
+++ quizgame.top-frontend/quizgame.top/src/assets/components/NavBar.tsx
@@ -2,6 +2,8 @@ import '../css/Navbar.scss';
 
 import title from '../images/quizgame-title-LD.png';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import { AiTwotoneSetting } from "react-icons/ai";
 import { AiOutlineClose } from "react-icons/ai";
 
@@ -22,16 +24,26 @@ const Navbar = () => {
     <div className='navbar-container'>
       <div className="navbar-content">
         <div className='navbar-logo-container'>
-          <a href='/'><img className='navbar-logo' src={title}/></a>
+          <Link to='/'><img className='navbar-logo' src={title}/></Link>
         </div>
         <div className='navbar-settings-icon-container'>
           <AiTwotoneSetting className='navbar-settings-icon' onClick={openSettings}/>
         </div> 
       </div>
       <div className={`navbar-settings-drawer-container ${open}`}>
-        <div className={`navbar-settings-drawer-overlay ${open}`}></div>
+        <div className={`navbar-settings-drawer-overlay ${open}`} onClick={closeSettings}></div>
         <div className={`navbar-settings-drawer ${open}`}>
-          <AiOutlineClose className='navbar-settings-close-button' onClick={closeSettings}/>
+          <div className='navbar-settings-header'>
+            <div className='navbar-settings-title'></div> 
+            <AiOutlineClose className='navbar-settings-close-button' onClick={closeSettings}/>
+          </div>
+          
+          <div className='navbar-settings-body'>
+            <div className='navbar-settings-user-auth'>
+              <Link to='/login' className='navbar-settings-login' onClick={closeSettings}>Log in</Link>
+              <Link to='/signup' className='navbar-settings-signup' onClick={closeSettings}>Sign up</Link>
+            </div>
+          </div>
         </div>
       </div> 
     </div> 
