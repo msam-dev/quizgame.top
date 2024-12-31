a08250ce1591a701b12287d36f9b085ad1ba7531
mSam-Dev <sam.engineer0@gmail.com>
2024-12-31 11:27:24 +1100

    Cleaning up some formatting

--- quizgame.top-frontend/quizgame.top/src/pages/Home.tsx
+++ quizgame.top-frontend/quizgame.top/src/pages/Home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
       <div className="home-welcome-card">
           Games:
         <a href='/world-flag-quiz-mc'>Flags Of The World (Multiple Choice)</a>
+        <a href='/test'>API testing</a>
     
       </div> 
     </div> 
