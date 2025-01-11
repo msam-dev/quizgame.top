3a2e6afabb90f434d3af7c546b32dd3715b480c8
mSam-Dev <sam.engineer0@gmail.com>
2025-01-11 17:47:08 +1100

    implementing basic quizlist page

--- quizgame.top-frontend/quizgame.top/src/pages/QuizList.tsx
+++ quizgame.top-frontend/quizgame.top/src/pages/QuizList.tsx
@@ -1,10 +1,24 @@
+import { Link } from 'react-router-dom';
 import '../assets/css/QuizList.scss';
 
 const QuizList = () => {
   return (
-    <div className='login-container'>
-      <div className='login-inner-container'>
-      QuizList
+    <div className='quizlist-container'>
+      <div className='quizlist-inner-container'>
+        <div className='quizlist-title'>All Games:</div>
+        <div className='quizlist-links'>
+          <div className='quizlist-link-container'> • <Link to='/World-Flag-Quiz-MC'        className='quizlist-link'>Flags of the World, Multiple Choice, Endless Mode</Link></div>
+          <div className='quizlist-link-container'> • <Link to='/Africa-Flag-Quiz-MC'       className='quizlist-link'>Flags of Africa, Multiple Choice, Endless Mode</Link></div>
+          <div className='quizlist-link-container'> • <Link to='/Asia-Flag-Quiz-MC'         className='quizlist-link'>Flags of Asia, Multiple Choice, Endless Mode</Link></div>
+          <div className='quizlist-link-container'> • <Link to='/Australasia-Flag-Quiz-MC'  className='quizlist-link'>Flags of Australasia, Multiple Choice, Endless Mode</Link></div>
+          <div className='quizlist-link-container'> • <Link to='/Europe-Flag-Quiz-MC'       className='quizlist-link'>Flags of Europe, Multiple Choice, Endless Mode</Link></div>
+          <div className='quizlist-link-container'> • <Link to='/NorthAmerica-Flag-Quiz-MC' className='quizlist-link'>Flags of North America, Multiple Choice, Endless Mode</Link></div>
+          <div className='quizlist-link-container'> • <Link to='/SouthAmerica-Flag-Quiz-MC' className='quizlist-link'>Flags of South America, Multiple Choice, Endless Mode</Link></div>
+
+          
+          
+        
+        </div>
       </div>
     </div> 
   );
