cf2bcd830142dbb704528ee3246efb8f3c8365af
mSam-Dev <sam.engineer0@gmail.com>
2025-01-12 11:36:27 +1100

    removing dead code

--- quizgame.top-frontend/quizgame.top/src/assets/components/NavBar.tsx
+++ quizgame.top-frontend/quizgame.top/src/assets/components/NavBar.tsx
@@ -2,16 +2,13 @@ import '../css/Navbar.scss';
 import title from '../images/quizgame-title-LD.png';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { AiTwotoneSetting } from 'react-icons/ai';
-import { AiOutlineClose } from 'react-icons/ai';
+import { AiTwotoneSetting, AiOutlineClose } from 'react-icons/ai';
 import { useQuizGameContext } from './QuizGameContext';
-import { App } from 'antd';
 
 const Navbar = () => {
 
   const navigate = useNavigate();
   const context = useQuizGameContext();
-  const { message } = App.useApp();
   const [open, setOpen] = useState<boolean>(false);
 
   const logout = () => {
@@ -34,7 +31,7 @@ const Navbar = () => {
         <div className={`navbar-settings-drawer-overlay ${open}`} onClick={() => setOpen(false)}></div>
         <div className={`navbar-settings-drawer ${open}`}>
           <div className='navbar-settings-header'>
-            <div className='navbar-settings-title'></div> 
+            <div className='navbar-settings-title'>user: {context.username} </div> 
             <AiOutlineClose className='navbar-settings-close-button' onClick={() => setOpen(false)}/>
           </div>
           <div className='navbar-settings-body'>
@@ -43,13 +40,6 @@ const Navbar = () => {
               <Link to='/login' className={`navbar-settings-login ${context.loggedIn}`} onClick={() => setOpen(false)}>Log in</Link>
               <Link to='/signup' className={`navbar-settings-signup ${context.loggedIn}`} onClick={() => setOpen(false)}>Sign up</Link>
             </div>
-            
-            <br/><br/>
-            <Link to='/world-flag-quiz-mc'>Flags Of The World (Multiple Choice)</Link><br/>
-            <Link to='/test'>API testing</Link><br/>
-            <Link to='/login'>Log in</Link><br/>
-            <Link to='/signup'>Sign up</Link><br/>
-            <Link to='/signup'>Full List of Games</Link><br/>
           </div>
         </div>
       </div> 
