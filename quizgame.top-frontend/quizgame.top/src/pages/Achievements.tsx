7c4aa7ffb7778c8c14d5d98238590e9e676d2644
mSam-Dev <sam.engineer0@gmail.com>
2025-01-15 21:19:14 +1100

    early stage mockups for achievements

--- quizgame.top-frontend/quizgame.top/src/pages/Achievements.tsx
+++ quizgame.top-frontend/quizgame.top/src/pages/Achievements.tsx
@@ -1,10 +1,56 @@
 import '../assets/css/Achievements.scss';
+import { useQuizGameContext } from '../assets/components/QuizGameContext';
+import { FaMedal } from "react-icons/fa";
+//import { Modal } from 'antd';
 
 const Achievements = () => {
+  // TODO: this needs be improved so that it generates all the achievements dynamically 
+  // from a json file so thats its easy to add new achievements
+  const context = useQuizGameContext();
+  context;
   return (
-    <div className='login-container'>
-      <div className='login-inner-container'>
-        Under Construction
+    <div className='achievements-container'>
+      <div className='achievements-inner-container'>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>Title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
+        <div className='achievements-medal-container'>
+          <div className='achievements-icon-container'><FaMedal className='achievements-icon'/></div>
+          <div className='achievements-medal-title'>title</div>
+        </div>
       </div>
     </div> 
   );
